c09b507584a7e57c0c01bf2f7a91daecc809769f
Edenilson Oliveira <edenilsonoliveira270@gmail.com>
2024-01-22 21:46:23 -0300

    Adding new orm sequence because prism does not work due to incompatibility with android

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,4 @@
 import express from 'express'
 const router = express.Router()
-import { UserGet,UserAdd } from '../controllers/UserController.js'
-
-router.get('/users', UserGet)
-router.post('/users', UserAdd)
 
 export default router
